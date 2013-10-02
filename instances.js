f20c58351c2e595aa629304238c8e11036d1539a
mkrause <maikelkrause@gmail.com>
2013-10-02 12:23:28 +0200

    Implement some modules.

---
+++ instances.js
@@ -0,0 +1,5 @@
+// List of known instances used to bootstrap the RM
+exports.instances = [
+    { host: "localhost", port: "8001" },
+    { host: "localhost", port: "8002" }
+];
---
+++ server.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var ResourceManager = require('lib/rm.js');
+var resolve = require('lib/resolve.js');
+
+// Initialize the resource manager
+var rm = new ResourceManager();
+var initialInstances = require('instances');
+rm.bootstrap(initialInstances);
+
+// Start proxy server
+var server = httpProxy.createServer(function(req, res, proxy) {
+    try {
+        var instance = resolve(req, rm);
+    } catch (e) {
+        res.writeHead(500, {
+            "Content-Type": "text/plain"
+        });
+        
+        res.end("Fail whale");
+    }
+    
+    proxy.proxyRequest(req, res, {
+        host: instance.host,
+        port: instance.port
+    });
+}).listen(8000);
+
+server.proxy.on('proxyError', function(err, req, res) {
+    res.writeHead(500, {
+        "Content-Type": "text/plain"
+    });
+    
+    res.end("Proxy error\n");
+    
+    rm.fire('serverFailure', req);
+});
