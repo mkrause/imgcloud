37c03598053ece30e0f4fe207c9ea4cb0d00321d
Yorick Holkamp <github@nextpulse.nl>
2013-10-03 12:58:51 +0200

    Extended the server.js class, finally discovered a way to get the proxy hostname/port upon error events

--- server.js
+++ server.js
@@ -6,33 +6,39 @@ var resolve = require('./lib/resolve.js');
 // Initialize the resource manager
 var rm = new ResourceManager("bar");
 var initialInstances = require('./instances');
-console.log(initialInstances);
 rm.bootstrap(initialInstances);
 
 // Start proxy server
 var server = httpProxy.createServer(function(req, res, proxy) {
     try {
         var instance = resolve(req, rm);
+
+        console.log("Proxied to " + instance.toString())
+
+        proxy.proxyRequest(req, res, {
+            host: instance.host,
+            port: instance.port
+        });
     } catch (e) {
         res.writeHead(500, {
             "Content-Type": "text/plain"
         });
         
-        res.end("Fail whale");
+        res.end("Fail whale:\n" + e.stack + "\n");
     }
-    
-    proxy.proxyRequest(req, res, {
-        host: instance.host,
-        port: instance.port
-    });
 }).listen(8000);
+
+server.proxy.on('end', function(req, res, response) {
+    console.log("Proxy end:\n")
+    console.log(response.headers);
+});
 
 server.proxy.on('proxyError', function(err, req, res) {
     res.writeHead(500, {
         "Content-Type": "text/plain"
     });
-    
-    res.end("Proxy error\n");
-    
-    rm.fire('serverFailure', req);
+
+    res.end("Proxy error while redirecting request to " + Object.keys(this.proxies)[0] +"\n");
+
+    rm.emit('serverFailure', req);
 });
