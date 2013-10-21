065f26051afb5118a957d90f89e30b3f807ed560
Yorick Holkamp <github@nextpulse.nl>
2013-10-21 21:02:19 +0200

    Randomized the loadbalancer for easier testing

--- server/lib/balancer.js
+++ server/lib/balancer.js
@@ -1,6 +1,7 @@
 exports.balance = function(rm) {
     var sorted_instances = rm.getInstances().sort(function(a, b) {
-        return a.load - b.load;
+//        return a.load - b.load;
+        return 0.5 - Math.random();
     });
 
     return sorted_instances[0];
