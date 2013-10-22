632171415e364b91a3cbb5adc7d72cd13ca48ae4
mkrause <maikelkrause@gmail.com>
2013-10-22 17:16:20 +0200

    Initial implementation of the DigitalOcean client.

--- server/lib/digital_ocean.js
+++ server/lib/digital_ocean.js
@@ -1,2 +1,30 @@
+var https = require('https');
+var querystring = require('querystring');
+var apiConfig = require('../api_key');
+
 module.exports = function DigitalOcean() {
+    function callApi(path, onSuccess, onError) {
+        var queryParams = apiConfig;
+        queryParams.name = 'foo'; // TODO
+        
+        var options = {
+            hostname: "api.digitalocean",
+            port: 443,
+            path: path + "?" + querystring.stringify(queryParams),
+            method: 'GET'
+        };
+        
+        var req = https.request(options, function(res) {
+            res.on('data', function(data) {
+                onSuccess(JSON.parse(data));
+            })
+        });
+        req.on('error', onError);
+    }
+    
+    this.allocate = function() {
+        callApi('/droplets/new', function(response) {
+            
+        });
+    };
 };
