a438389537cb9b1febb74cd2bdb6366558dbd28a
mkrause <maikelkrause@gmail.com>
2013-10-23 16:29:08 +0200

    Implement basic provisioning.

--- server/lib/rm.js
+++ server/lib/rm.js
@@ -2,8 +2,40 @@ var http = require('q-io/http');
 var Instance = require('./instance.js');
 var DigitalOcean = require('./digital_ocean.js');
 
+// Get the average of a numerical array
+function average(arr) {
+    var sum = arr.reduce(function(prev, cur) {
+        return prev + cur;
+    }, 0);
+    
+    var avg;
+    if (arr.length == 0) {
+        avg = 0;
+    } else {
+        avg = sum / arr.length;
+    }
+    
+    return avg;
+}
+
 module.exports = function ResourceManager() {
     var self = this;
+    
+    // Number of milliseconds between each uptime check
+    this.POLL_FREQUENCY = 5 * 1000;
+    
+    // Number of milliseconds between each provisioning
+    this.PROVISION_FREQUENCY = 30 * 1000;
+    
+    this.LOAD_HISTORY_WINDOW_SIZE = parseInt(this.PROVISION_FREQUENCY / this.POLL_FREQUENCY, 10);
+    
+    // Bounds on the number of limits
+    this.MIN_INSTANCES = 2;
+    this.MAX_INSTANCES = 10;
+    
+    // Thresholds for allocating or deallocating instances
+    this.ALLOCATION_THRESHOLD = 0.80;
+    this.DEALLOCATION_THRESHOLD = 0.50;
     
     // Client for our IaaS provider (DigitalOcean)
     this.digitalOcean = new DigitalOcean(require('../digital_ocean_config.js'));
@@ -16,9 +48,6 @@ module.exports = function ResourceManager() {
     
     // Some port we know to be available (useful for creating local test this.instances)
     this.availablePort = 8001;
-    
-    // Number of milliseconds between each uptime check
-    this.POLL_FREQUENCY = 5000;
     
     this.allocateInstance = function() {
         /*
@@ -47,13 +76,24 @@ module.exports = function ResourceManager() {
         return this.digitalOcean.deallocate(instance);
     };
 
-    this.markInstanceDead = function(instance_obj) {
-        console.log("markInstanceDead: marking %s", instance_obj);
+    this.markInstanceDead = function(instance) {
+        console.log("markInstanceDead: marking %s", instance);
+        
         // Remove instance
-        var instance = this.getInstance(instance_obj);
         var index = this.instances.indexOf(instance);
         if (index != -1) {
             this.instances.splice(index, 1);
+        }
+        
+        this.deallocateInstance(instance)
+            .then(function() {
+                console.log("Succesfully deallocated: " + instances);
+            })
+            .fail(console.error);
+        
+        // Check if we need to allocate a new instance to make up
+        if (this.instances.length < this.MIN_INSTANCES) {
+            this.allocateInstance();
         }
     };
 
@@ -72,13 +112,26 @@ module.exports = function ResourceManager() {
         return instance;
     };
     
+    this.pingInstance = function(instance) {
+        return http.request("http://" + instance.host + ":" + instance.port + "/ping");
+    };
+    
     this.setInstanceLoad = function(host, port, load) {
         var instance = this.getInstance({host: host, port: port})
         instance.load = load.split(",")[0];
     };
     
-    this.pingInstance = function(instance) {
-        return http.request("http://" + instance.host + ":" + instance.port + "/ping");
+    this.recordInstanceLoad = function(instance) {
+        if (!instance.loadHistory) {
+            instance.loadHistory = [];
+        }
+        
+        // Remove the first element, if we've exceeded the window size
+        if (instance.loadHistory.length >= this.LOAD_HISTORY_WINDOW_SIZE) {
+            instance.loadHistory.shift();
+        }
+        
+        instance.loadHistory.push(instance.load);
     };
     
     this.pollInstances = function() {
@@ -86,14 +139,39 @@ module.exports = function ResourceManager() {
         this.instances.forEach(function(instance) {
             this.pingInstance(instance)
                 .then(function(data) {
-                    instance.load = data.headers['x-imgcloud-load'].split(',')[0];
+                    instance.load = Number(data.headers['x-imgcloud-load'].split(',')[0]);
                     console.log("pollInstances: %s is alive", instance);
+                    
+                    // Save the load for this instance in its history
+                    self.recordInstanceLoad(instance);
                 })
                 .fail(function() {
                     console.log("pollInstances: %s died", instance);
-                    this.markInstanceDead(instance);
+                    self.markInstanceDead(instance);
                 });
         }, this);
+    };
+    
+    this.calculateSystemLoad = function() {
+        var instanceLoads = [];
+        this.instances.forEach(function(instance) {
+            instanceLoads.push(average(instance.loadHistory || []));
+        }, this);
+        
+        return average(instanceLoads);
+    };
+    
+    // Provision (allocate or deallocate) resources based on the system load
+    this.provision = function() {
+        var systemLoad = this.calculateSystemLoad();
+        console.log("System load: " + systemLoad);
+        
+        var numInstances = this.instances.length;
+        if (systemLoad > this.ALLOCATION_THRESHOLD && numInstances < this.MAX_INSTANCES) {
+            this.allocateInstance();
+        } else if (systemLoad < this.DEALLOCATION_THRESHOLD && numInstances > this.MIN_INSTANCES) {
+            this.deallocateInstance();
+        }
     };
     
     // Bootstrap the resource manager, with an optional set of initial instances
@@ -113,6 +191,9 @@ module.exports = function ResourceManager() {
         
         // Start polling
         setInterval(this.pollInstances.bind(this), this.POLL_FREQUENCY);
+        
+        // Check the load at regular intervals, and provision accordingly
+        setInterval(this.provision.bind(this), this.PROVISION_FREQUENCY);
     };
     
     // Emit an event
