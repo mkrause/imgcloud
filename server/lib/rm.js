d1bca94fd88741bc5f6cb0d1da1dafcb100364d9
Yorick Holkamp <github@nextpulse.nl>
2013-10-25 11:48:14 +0200

    Patched up the stats recording, now including leading zeros

--- server/lib/rm.js
+++ server/lib/rm.js
@@ -11,78 +11,78 @@ module.exports = function ResourceManager() {
 
     // Redis database used for storage
     this.db = redis.createClient();
-    
+
     // Client for our IaaS provider (DigitalOcean)
     if (config.useDigitalOcean) {
         this.digitalOcean = new DigitalOcean(config.digitalOcean);
     } else {
         this.digitalOcean = new DigitalOceanFake();
     }
-    
+
     // List of application instances
     this.instances = [];
-    
+
     // Some ID that is guaranteed to be currently available
     this.availableId = 1;
-    
+
     // Some port we know to be available (useful for creating local test instances)
     this.availablePort = 8001;
-    
+
     this.allocateInstance = function() {
         // Enforce upper bound on the number of instances
         var numInstances = this.instances.length;
         var canAllocate = numInstances < config.MAX_INSTANCES;
-        
+
         if (!canAllocate) {
             console.log("allocateInstance: did not allocate, upper bound reached");
             return;
         }
-        
+
         var id = this.availableId;
         console.log("Allocating instance with ID: " + id);
-        
+
         this.availableId += 1;
         return this.digitalOcean.allocate(id);
     };
-    
+
     this.deallocateInstance = function(instance) {
         // Enforce lower bound on the number of instances
         var numInstances = this.instances.length;
         var canDeallocate = numInstances > config.MIN_INSTANCES;
-        
+
         if (!canDeallocate) {
             console.log("deallocateInstance: did not deallocate, lower bound reached");
             return;
         }
-        
+
         console.log("Deallocating instance: " + instance);
         return this.digitalOcean.deallocate(instance);
     };
-    
+
     this.addInstance = function(instance) {
         this.instances.push(instance);
     };
-    
+
     this.removeInstance = function(instance) {
         var index = this.instances.indexOf(instance);
         if (index != -1) {
             this.instances.splice(index, 1);
         }
     };
-    
+
     this.markInstanceDead = function(instance) {
         console.log("markInstanceDead: marking %s", instance);
         instance.notifyDead();
-        
+
         // Remove instance
         this.removeInstance(instance);
-        
+
         this.deallocateInstance(instance)
             .then(function() {
                 console.log("Succesfully deallocated: " + instances);
             })
             .fail(console.error);
-        
+
         // Check if we need to allocate a new instance to make up for the deallocated one
         if (this.instances.length < config.MIN_INSTANCES) {
             this.allocateInstance();
@@ -92,13 +92,13 @@ module.exports = function ResourceManager() {
     this.getInstances = function() {
         return this.instances;
     };
-    
+
     this.getRunningInstances = function() {
         return this.instances.filter(function(instance) {
             return instance.isRunning();
         });
     };
-    
+
     this.getInstance = function(id) {
         var instance = false;
         this.instances.forEach(function(inst) {
@@ -107,10 +107,10 @@ module.exports = function ResourceManager() {
                 return false; // Break
             }
         }, this);
-        
+
         return instance;
     };
-    
+
     this.pingInstance = function(instance) {
         return http.request("http://" + instance.host + ":" + instance.port + "/ping");
     };
@@ -129,36 +129,36 @@ module.exports = function ResourceManager() {
                         console.log("pollInstances: %s has not booted yet", instance);
                         return;
                     }
-                    
+
                     console.log("pollInstances: %s died", instance);
                     self.markInstanceDead(instance);
                 });
         }, this);
     };
-    
+
     this.startPolling = function(freq) {
         freq = freq || config.POLL_FREQUENCY;
         setInterval(this.pollInstances.bind(this), freq);
     };
-    
+
     // Calculate the average load over instances in the system
     this.calculateSystemLoad = function() {
         var instanceLoads = [];
         this.instances.forEach(function(instance) {
             instanceLoads.push(instance.averageLoad());
         }, this);
-        
+
         return average(instanceLoads);
     };
-    
+
     // Provision (allocate or deallocate) resources based on the system load
     this.provision = function() {
         var systemLoad = 20 * Math.random();//this.calculateSystemLoad(); //TODO
         console.log("System load: " + systemLoad);
-        
+
         if (systemLoad > config.ALLOCATION_THRESHOLD) {
             console.log("provision: allocate");
-            
+
             var id = this.availableId++;
             this.allocateInstance(id)
                 .then(function(instance) {
@@ -167,7 +167,7 @@ module.exports = function ResourceManager() {
                 .fail(console.error);
         } else if (systemLoad < config.DEALLOCATION_THRESHOLD) {
             console.log("provision: deallocate");
-            
+
             // Randomly kill some instance
             var victim = this.getRunningInstances()[0];
             this.removeInstance(victim);
@@ -176,33 +176,37 @@ module.exports = function ResourceManager() {
             console.log("provision: all is well");
         }
     };
-    
+
     this.startProvisioning = function(freq) {
         freq = freq || config.PROVISION_FREQUENCY;
-        
+
         // Check the load at regular intervals, and provision accordingly
         setInterval(this.provision.bind(this), freq);
     };
-    
+
     // Bootstrap the resource manager, with an optional set of initial instances
     this.bootstrap = function(initialInstances) {
         initialInstances = initialInstances || [];
-        
+
         this.instances = [];
         initialInstances.forEach(function(instInfo) {
             var inst = new Instance(instInfo.id, instInfo.host, instInfo.port);
             this.addInstance(inst);
-            
+
             // Make sure our availableId is higher
             this.availableId = Math.max(this.availableId, instInfo.id + 1);
         }, this);
-        
+
         console.log("Initialized RM (with %d bootstrap instances)", this.instances.length);
     };
-    
+
     this.saveRequestStats = function(instanceId, res) {
+        var format = function(num) {
+            return num < 10 ? "0" + num : num;
+        }
+
         var curTime = new Date;
-        var keyBit = instanceId + "-"+curTime.getHours() + ":"+ curTime.getMinutes() + ":" + curTime.getSeconds();
+        var keyBit = instanceId + "-"+format(curTime.getHours()) + ":"+ format(curTime.getMinutes()) + ":" + format(curTime.getSeconds());
 
         var values = {}
 
@@ -218,7 +222,7 @@ module.exports = function ResourceManager() {
 
         this.db.mset(values);
     };
-    
+
     // Emit an event
     this.emit = function(eventName, req, res) {
         console.log("Received " + eventName);
