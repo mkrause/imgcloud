3aeff262fdd2c36784a29d532a38b9a73716057b
Yorick Holkamp <github@nextpulse.nl>
2013-10-25 11:45:07 +0200

    Patched up the stats output

--- imgcloud/routes/index.js
+++ imgcloud/routes/index.js
@@ -1,4 +1,3 @@
-var os = require('os');
 var redis = require("then-redis");
 
 /*
@@ -12,45 +11,34 @@ exports.ping = function (req, res) {
     res.send('200')
 };
 
-// Get the average of a numerical array
-function average(arr) {
-    var sum = arr.reduce(function(prev, cur) {
-        return prev + cur;
-    }, 0);
-
-    var avg;
-    if (arr.length == 0) {
-        avg = 0;
-    } else {
-        avg = sum / arr.length;
-    }
-
-    return avg;
-}
-
-exports.stats = function(req, res) {
+exports.stats = function (req, res) {
     var DB = redis.createClient();
 
-    var os_loads = {};
+    var stats = {};
 
-    DB.keys("imgcloud-osload-*").then(function(keys) {
-        DB.send("mget", keys).then(function(values) {
-            for(var i=0; i < keys.length; i++) {
+    DB.keys("imgcloud-*").then(function (keys) {
+        var filtered_keys = keys.filter(function(value) {
+            var fragment = value.split("-")[1];
+            return ["osload", "response"].indexOf(fragment) != -1;
+        });
+        DB.send("mget", filtered_keys).then(function (values) {
+            for (var i = 0; i < keys.length; i++) {
                 var key = keys[i].split("-");
                 var instance = key[2];
                 var time = key[3];
 
-                if(!os_loads[time]) {
-                    os_loads[time] = {}
+                if(!stats[key[1]]) {
+                    stats[key[1]] = {};
                 }
-                os_loads[time][instance] = values[i];
+                if (!stats[key[1]][time]) {
+                    stats[key[1]][time] = {};
+                }
+
+                stats[key[1]][time][instance] = values[i];
             }
 
-            console.log(os_loads);
-
-            res.write(JSON.stringify(os_loads));
+            res.write(JSON.stringify(stats));
             res.end();
         });
     });
-    var response_time_keys = DB.keys("imgcloud-response-*");
 };
