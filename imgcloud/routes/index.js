cd847785990a58d4f265f25cada3a37789948259
Yorick Holkamp <github@nextpulse.nl>
2013-10-25 12:05:35 +0200

    Included a time-based stats filter to reduce the output size

--- imgcloud/routes/index.js
+++ imgcloud/routes/index.js
@@ -11,19 +11,39 @@ exports.ping = function (req, res) {
     res.send('200')
 };
 
+/**
+ * Statistics endpoint, accessible through /stats, with optional ?from=12:35:45 time filter
+ */
 exports.stats = function (req, res) {
     var DB = redis.createClient();
-
     var stats = {};
+
+    var strToTime = function(timeString) {
+        date = new Date;
+        date.setHours(timeString.split(":")[0]);
+        date.setMinutes(timeString.split(":")[1]);
+        date.setSeconds(timeString.split(":")[2]);
+        return date;
+    }
+
+    var fromTime;
+    if(req.query["from"]) {
+        fromTime = strToTime(req.query["from"]);
+    }
 
     DB.keys("imgcloud-*").then(function (keys) {
         var filtered_keys = keys.filter(function(value) {
-            var fragment = value.split("-")[1];
-            return ["osload", "response"].indexOf(fragment) != -1;
+            var fragments = value.split("-");
+            var time = true;
+            if(fromTime) {
+                time = strToTime(fragments[3]) > fromTime;
+                console.log("%s > %s = %s", strToTime(fragments[3]), fromTime, time)
+            }
+            return time && ["osload", "response"].indexOf(fragments[1]) != -1;
         });
         DB.send("mget", filtered_keys).then(function (values) {
-            for (var i = 0; i < keys.length; i++) {
-                var key = keys[i].split("-");
+            for (var i = 0; i < filtered_keys.length; i++) {
+                var key = filtered_keys[i].split("-");
                 var instance = key[2];
                 var time = key[3];
 
