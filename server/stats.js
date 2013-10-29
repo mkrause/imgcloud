3b1b780711e9d27afd214696390113e171da549a
Yorick Holkamp <github@nextpulse.nl>
2013-10-29 18:26:39 +0100

    Moved stats export to a separate script file

---
+++ server/stats.js
@@ -0,0 +1,38 @@
+var redis = require("then-redis");
+var DB = redis.createClient();
+var stats = {};
+
+var strToTime = function(timeString) {
+    date = new Date;
+    date.setHours(timeString.split(":")[0]);
+    date.setMinutes(timeString.split(":")[1]);
+    date.setSeconds(timeString.split(":")[2]);
+    return date;
+}
+
+DB.keys("imgcloud-*").then(function (keys) {
+    var filtered_keys = keys.filter(function(value) {
+        var fragments = value.split("-");
+        var time = true;
+        return time && ["osload", "response"].indexOf(fragments[1]) != -1;
+    });
+
+    DB.send("mget", filtered_keys).then(function (values) {
+        for (var i = 0; i < filtered_keys.length; i++) {
+            var key = filtered_keys[i].split("-");
+            var instance = key[2];
+            var time = key[3];
+
+            if(!stats[key[1]]) {
+                stats[key[1]] = {};
+            }
+            if (!stats[key[1]][time]) {
+                stats[key[1]][time] = {};
+            }
+
+            stats[key[1]][time][instance] = values[i];
+        }
+        console.log(JSON.stringify(stats));
+        process.exit(0);
+    });
+});
